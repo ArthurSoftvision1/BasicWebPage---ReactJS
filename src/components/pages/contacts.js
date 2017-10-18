2726687e9e1074d2577e613b6dbb4a19928f544b
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-18 17:39:20 +0300

    homePage/products/contacts component

---
+++ src/components/pages/contacts.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Contacts extends Component {
+  render() {
+    return (
+    <div className="container-fluid">
+        <h1>Contact Page</h1>
+        <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
+        Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
+        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+        Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. 
+        Etiam rhoncus.
+        </p>
+    </div>
+    );
+  }
+}
+
+export default Contacts;
---
+++ src/components/pages/products.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render() {
+    return (
+    <div className="container-fluid">
+        <h1>Products</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+        Aenean commodo ligula eget dolor. Aenean massa. 
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+        Nulla consequat massa quis enim. 
+        </p>
+    </div>
+    );
+  }
+}
+
+export default Products;
