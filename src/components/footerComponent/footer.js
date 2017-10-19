5f14ca16ec20482308128c72f958175cfb826297
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-19 11:33:37 +0300

    Include links in footer

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import Logo from '../headerComponent/logo';
+import {
+  Link // import Link
+} from 'react-router-dom'
 
-class Footer extends Component {
-  render() {
+class Footer extends Component { // footer component
+  render() { // render function
     return (
      <footer>
-            Footer
+        <div className="footer-section">
+
+        <div className="logo">
+        <div className="logo-position">
+            <Logo />
+        </div>
+    </div>
+
+            <ul className="footer-options">
+                <li className="first"> 
+                    <Link to="/" className="footer-links">Home</Link>
+                </li>
+                <li> 
+                    <Link to="/Products" className="footer-links">Products</Link>
+                </li>
+                <li className="last"> 
+                    <Link to="/Contacts" className="footer-links">Contact</Link>
+                </li>
+            </ul>
+        </div>
      </footer>
     );
   }
 }
 
-export default Footer;
+export default Footer; // export Footer component
