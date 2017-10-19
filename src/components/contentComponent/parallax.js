b583fa5ecf3e79b4fc52d85bb58f619a6bf8af7c
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-19 17:18:11 +0300

    Parallax component

---
+++ src/components/contentComponent/parallax.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Parallax extends Component {
+  render() {
+    return (
+        <div className="parallax"> 
+
+        </div>
+
+    );
+  }
+}
+
+export default Parallax;
