e5b1b01f505ff4ee0a1a3abcbde864964174e09a
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-19 14:20:01 +0300

    Search input component

---
+++ src/components/headerComponent/searchInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+        <input type="text" class="form-control input-lg" id="searchBar" placeholder="Search" /> 
+    );
+  }
+}
+
+export default Search;
