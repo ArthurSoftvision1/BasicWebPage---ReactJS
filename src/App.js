3c9369e798e026af9657e4d1380fa7f64dc69a64
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-19 13:46:52 +0300

    Rest of the files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route, // import the Route for creating routes
+  Link // import the Link for creating Links
 } from 'react-router-dom';
 
 import './style.css';
@@ -10,24 +10,25 @@ import Header from './components/headerComponent/header';
 import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
 import Products from './components/pages/products';
-import Contacts from './components/pages/contacts';
+import Contact from './components/pages/contacts';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-       <Header />
-        
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/Products" component={Products} />
-        <Route exact path="/Contacts" component={Contacts} />
+        <div className="App">
+        <Header />
+          
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/Products" component={Products} />
+          <Route exact path="/Contacts" component={Contact} />
 
-       <Footer />
-      </div>
+        <Footer />
+        </div>
       </Router>
     );
   }
 }
 
-export default App;
+export default App; // export App component
