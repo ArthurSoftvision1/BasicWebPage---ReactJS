d86a23ac1d3925b72ad5da0769fdf15e317f46a0
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-20 17:14:11 +0300

    ParallaxBlock component changes

--- src/components/contentComponent/parallaxBlock.js
+++ src/components/contentComponent/parallaxBlock.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import RegisterButton from '../pages/registerButton';
 
 class ParallaxBlock extends Component {
   render() {
     return (
+        
         <div class="parallax-block">
-          <h1>Home Page Content</h1>
+        <RegisterButton />
+          <h1>First Page Content</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+              officia deserunt mollit anim id est laborum.
+          </p>
+          
+          <h1>Second Page Content</h1>
+          <p>Sed in arcu ullamcorper purus condimentum convallis. Vestibulum est odio, commodo fermentum ipsum vel, lobortis hendrerit mauris. 
+          Mauris vel lacinia dolor. Integer viverra neque vel efficitur molestie. 
+          In dictum lorem vitae convallis interdum. Suspendisse auctor ex varius lorem egestas consequat.
+          </p>
+
+          <h1>Third Page Content</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+              officia deserunt mollit anim id est laborum.
+          </p>
+
+          <h1>Fourth Page Content</h1>
+          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur quam dolor, pharetra nec iaculis non, aliquet eget massa. 
+          Nulla sit amet elit et nisi ultrices aliquet. Morbi ornare tempor pellentesque. Morbi elementum sapien nibh, id imperdiet purus consectetur vitae. 
+          Vestibulum eu risus gravida magna facilisis sollicitudin. 
+          Donec vitae ultrices odio. Aenean quis augue at sem efficitur elementum tincidunt sed ante. In fringilla sollicitudin diam ac viverra.
+          </p>
+
+          <h1>Fifth Page Content</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+              officia deserunt mollit anim id est laborum.
+          </p>
+
+          <h1>Sixth Page Content</h1>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+          totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          </p>
+
+          <h1>Seventh Page Content</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
