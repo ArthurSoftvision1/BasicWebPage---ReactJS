a9ce7489d354ef3e6c72f9babe236ee7a9627b05
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-20 17:09:33 +0300

    Include Login and Register page in App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
 import Products from './components/pages/products';
 import Contact from './components/pages/contacts';
+import RegisterPage from './components/pages/registerPage';
+import LogInPage from './components/pages/logInPage';
 
 class App extends Component {
   render() {
@@ -18,7 +20,9 @@ class App extends Component {
       <Router>
         <div className="App">
         <Header />
-          
+         
+          <Route exact path="/LoginPage" component={LogInPage} />
+          <Route exact path="/RegisterPage" component={RegisterPage} />
           <Route exact path="/" component={Homepage} />
           <Route exact path="/Products" component={Products} />
           <Route exact path="/Contacts" component={Contact} />
