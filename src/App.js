16266d2a36d13ca91d7aca7a8becbeeebaa34946
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-20 09:07:42 +0300

    Deleted files

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import {
   Link // import the Link for creating Links
 } from 'react-router-dom';
 
-import './style.css';
+
 import Header from './components/headerComponent/header';
 import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
 import Products from './components/pages/products';
 import Contact from './components/pages/contacts';
-
 
 class App extends Component {
   render() {
